ad5f0ace6d9d1bfce4020567e56796a323e0d08b
harpersgeekly <harperryanc@gmail.com>
2019-01-29 16:04:47 -0600

    added comment about of() "used RxJS of() to return an Observable of mock heroes (Observable<Hero[]>)."

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -21,7 +21,7 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return of(HEROES); // used RxJS of() to return an Observable of mock heroes (Observable<Hero[]>).
   }
 
 }
