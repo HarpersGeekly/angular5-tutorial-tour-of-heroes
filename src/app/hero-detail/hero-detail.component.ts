b3c0d2346568c44216897db115b565840808131d
harpersgeekly <harperryanc@gmail.com>
2019-01-30 12:44:08 -0600

    added a getHero() method that extracts the id from the path and gets the hero depending on that id added a goBack() method that sends the user back to the location they arrived from built in location.back()

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../models/hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,11 +11,34 @@ import { Hero } from '../models/hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    // The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent.
+    // This component is interested in the route's bag of parameters extracted from the URL.
+    // The "id" parameter is the id of the hero to display.
+    private heroService: HeroService,
+    // The HeroService gets hero data from the remote server and this component will use it to get the hero-to-display
+    private location: Location
+    // The location is an Angular service for interacting with the browser.
+  ) { }
 
   @Input() hero: Hero; // @Input() used for [hero] in heroes.component.html as: <app-hero-detail [hero]="selectedHero"></app-hero-detail>
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    // The route.snapshot is a static image of the route information shortly after the component was created.
+    // The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+    // Route parameters are always strings. The JavaScript (+) operator converts the string to a number, which is what a hero id should be.
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
