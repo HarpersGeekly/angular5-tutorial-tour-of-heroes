5b509bb2875fdf540ebbc32fbb6682a870b64f5e
harpersgeekly <harperryanc@gmail.com>
2019-01-30 16:02:45 -0600

    CRUD: fetches all Heroes, fetches one Hero, updateHero(), addHero(), deleteHero(), searchHero()

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -10,6 +10,10 @@ import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable() // The @Injectable() decorator tells Angular that this service might itself have injected dependencies.
 export class HeroService {
   // The HeroService could get hero data from anywhere—a web service, local storage, or a mock data source.
@@ -25,8 +29,6 @@ export class HeroService {
   // which is injected into the HeroesComponent.
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
     // return of(HEROES); // used RxJS of() to return an Observable of mock heroes (Observable<Hero[]>).
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe( // **pipe**
@@ -81,6 +83,64 @@ export class HeroService {
   // it constructs a request URL with the desired hero's id.
   // the server should respond with a single hero rather than an array of heroes.
   // therefore, getHero returns an Observable<Hero> ("an observable of Hero objects") rather than an observable of hero arrays .
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+  // The HttpClient.put() method takes three parameters
+    // -the URL
+    // -the data to update (the modified hero in this case)
+    // -options
+  // The URL is unchanged. The heroes web API knows which hero to update by looking at the hero's id.
+  // The heroes web API expects a special header in HTTP save requests.
+  // That header is in the httpOptions constant defined in the HeroService.
+
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  // HeroService.addHero() differs from updateHero in two ways.
+    // -it calls HttpClient.post() instead of put().
+    // -it expects the server to generate an id for the new hero, which it returns in the Observable<Hero> to the caller.
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+    // Note that
+      // -it calls HttpClient.delete.
+      // -the URL is the heroes resource URL plus the id of the hero to delete
+      // -you don't send data as you did with put and post.
+      // -you still send the httpOptions.
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(this.heroesUrl + `/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+  // The method returns immediately with an empty array if there is no search term.
+  // The rest of it closely resembles getHeroes().
+  // The only significant difference is the URL, which includes a query string with the search term.
+
 
   /**
    * Handle Http operation that failed.
